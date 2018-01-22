a98bc1984b115721aee49ec40f41c6bed9fb5402
sh1ch <isogucchi-code@outlook.com>
2018-01-22 13:58:05 +0900

    GridSplitter の位置の保存・復元。

---
+++ SaveGridSplitterSample/SaveGridSplitterSample/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using settings = SaveGridSplitterSample.Properties.Settings;
+
+namespace SaveGridSplitterSample
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Save();
+        }
+
+        private void Window_SourceInitialized(object sender, EventArgs e)
+        {
+            Restore();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Save();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Restore();
+        }
+
+        private void Save()
+        {
+            var heights = SplitGrid.RowDefinitions.Select(p => p.ActualHeight).ToArray();
+
+            settings.Default.GridRowHeight1 = heights[0];
+            settings.Default.GridRowHeight3 = heights[2];
+            settings.Default.Save();
+        }
+
+        private void Restore()
+        {
+            var height1 = settings.Default.GridRowHeight1;
+            var height3 = settings.Default.GridRowHeight3;
+
+            SplitGrid.RowDefinitions[0].Height = new GridLength(height1, GridUnitType.Star);
+            SplitGrid.RowDefinitions[2].Height = new GridLength(height3, GridUnitType.Star);
+        }
+
+    }
+}
