648578712115f85ac298e0884e92db6e596e4c70
sh1ch <isogucchi-code@outlook.com>
2018-05-23 10:22:27 +0900

    例外を発生させるサンプルプログラム。

---
+++ ExceptionTrapSample/ExceptionTrapSample/App.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ExceptionTrapSample
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        static App()
+        {
+            AppDomain.CurrentDomain.FirstChanceException += (sender, args) =>
+            {
+                ReportException("FirstChanceException.log", sender, args.Exception);
+            };
+
+            AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+            {
+                MessageBox.Show("未処理の例外が発生しました。アプリケーションを強制終了します。");
+                Current.Shutdown();
+            };
+        }
+
+        public App()
+        {
+            // UI スレッドの処理されない例外
+            DispatcherUnhandledException += (sender, args) =>
+            {
+                var name = args.Exception.TargetSite.Name;
+                var message = args.Exception.Message;
+
+                var result = MessageBox.Show($"例外が {name} で発生しました。プログラムを継続しますか。{Environment.NewLine}詳細:{message}", "DispatcherUnhandledException", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+                if (result == MessageBoxResult.Yes)
+                {
+                    args.Handled = true;
+                }
+            };
+
+        }
+
+        /// <summary>
+        /// 例外が発生したとき、ログファイルに例外情報を記録します。
+        /// </summary>
+        /// <param name="fileName">ログファイルの名前。</param>
+        /// <param name="sender">例外の発生対象。</param>
+        /// <param name="exception">発生した例外。</param>
+        private static void ReportException(string fileName, object sender, Exception exception)
+        {
+            const string reportFormat = "===========================================================\r\n" +
+                                        "ERROR Date = {0}, Sender = {1}, \r\n" +
+                                        "{2}\r\n\r\n";
+
+            var reportText = string.Format(reportFormat, DateTimeOffset.Now, sender, exception);
+            File.AppendAllText(fileName, reportText);
+        }
+
+    }
+}
---
+++ ExceptionTrapSample/ExceptionTrapSample/MainWindow.xaml.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExceptionTrapSample
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1A(object sender, RoutedEventArgs e)
+        {
+            // UI スレッドで例外を発生させる
+            throw new InvalidOperationException($"ThreadID:{Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        private void Button_Click_2A(object sender, RoutedEventArgs e)
+        {
+            int a = 0;
+            int b = 0;
+
+            var error = a / b; // 0 の除算
+        }
+
+        private void Button_Click_3A(object sender, RoutedEventArgs e)
+        {
+            var task = Task.Run(() => 
+            {
+                // UI スレッドで例外を発生させる
+                throw new InvalidOperationException($"ThreadID:{Thread.CurrentThread.ManagedThreadId}");
+            });
+
+            // 待たないとエラーを捕捉できない
+            task.Wait();
+        }
+
+        private async void Button_Click_4A(object sender, RoutedEventArgs e)
+        {
+            await Task.Run(() => 
+            {
+                throw new InvalidOperationException($"ThreadID:{Thread.CurrentThread.ManagedThreadId}");
+            });
+        }
+
+        private void Button_Click_1B(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // UI スレッドで例外を発生させる
+                throw new InvalidOperationException($"ThreadID:{Thread.CurrentThread.ManagedThreadId}");
+            }
+            catch
+            {
+                MessageBox.Show($"ThreadID:{Thread.CurrentThread.ManagedThreadId} 例外が発生しました。", "try-catch ステートメントより");
+            }
+        }
+
+        private void Button_Click_2B(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int a = 0;
+                int b = 0;
+
+                var error = a / b; // 0 の除算
+            }
+            catch
+            {
+                MessageBox.Show($"ThreadID:{Thread.CurrentThread.ManagedThreadId} 例外が発生しました。", "try-catch ステートメントより");
+            }
+        }
+
+        private void Button_Click_3B(object sender, RoutedEventArgs e)
+        {
+            var task = Task.Run(() =>
+            {
+                // UI スレッドで例外を発生させる
+                throw new InvalidOperationException($"ThreadID:{Thread.CurrentThread.ManagedThreadId}");
+            });
+
+            try
+            {
+                task.Wait();
+            }
+            catch
+            {
+                MessageBox.Show($"ThreadID:{Thread.CurrentThread.ManagedThreadId} 例外が発生しました。", "try-catch ステートメントより");
+            }
+
+        }
+
+        private async void Button_Click_4B(object sender, RoutedEventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                try
+                {
+                    // UI スレッドで例外を発生させる
+                    throw new InvalidOperationException($"ThreadID:{Thread.CurrentThread.ManagedThreadId}");
+                }
+                catch
+                {
+                    MessageBox.Show($"ThreadID:{Thread.CurrentThread.ManagedThreadId} 例外が発生しました。", "try-catch ステートメントより");
+                }
+            });
+        }
+    }
+}
