92e9387000573ae44d37d0d535ff11bed4bbecaf
sh1ch <isogucchi-code@outlook.com>
2019-02-04 18:26:37 +0900

    Googleカレンダーの情報を取得するサンプル (Google Calendar API v3)

--- GCP_TextToSpeech_Sample/GCP_TextToSpeech_Sample/MainWindow.xaml.cs
+++ GCP_TextToSpeech_Sample/GCP_TextToSpeech_Sample/MainWindow.xaml.cs
@@ -30,6 +30,35 @@ namespace GCP_TextToSpeech_Sample
             _Client = TextToSpeechClient.Create();
 
             InitializeComponent();
+
+            var settings = Properties.Settings.Default;
+
+            // 設定の読み込み
+            UseWaveNet.IsChecked = settings.UseWaveNet;
+            SpeedSlider.Value = settings.Speed;
+            PitchSlider.Value = settings.Pitch;
+
+            UseAutoIncrement.IsChecked = settings.UseAutoIncrement;
+            SceneID.Text = settings.SceneID;
+            TextID.Text = $"{settings.TextID:000}";
+            RetakeID.Text = settings.RetakeID;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var settings = Properties.Settings.Default;
+
+            // 設定の保存
+            settings.UseWaveNet = UseWaveNet.IsChecked ?? false;
+            settings.Speed = SpeedSlider.Value;
+            settings.Pitch = PitchSlider.Value;
+
+            settings.UseAutoIncrement = UseAutoIncrement.IsChecked ?? false;
+            settings.SceneID = SceneID.Text;
+            settings.TextID = Convert.ToInt32(TextID.Text);
+            settings.RetakeID = RetakeID.Text;
+
+            settings.Save();
         }
 
         private void Button_Click_Save(object sender, RoutedEventArgs e)
@@ -54,11 +83,36 @@ namespace GCP_TextToSpeech_Sample
             };
 
             var response = _Client.SynthesizeSpeech(input, voiceSection, audioConfig);
+            var fileName = GetFileName();
 
-            using (var output = File.Create("output.mp3"))
+            using (var output = File.Create($"mp3\\{fileName}"))
             {
                 response.AudioContent.WriteTo(output);
             }
+
+            // 画面を更新
+            UpdateDisplayValues(fileName);
+        }
+
+        private string GetFileName()
+        {
+            var sceneID = SceneID.Text;
+            var textID = TextID.Text;
+            var retakeID = RetakeID.Text;
+            var text = SpeechText.Text.Length > 5 ? SpeechText.Text.Substring(0, 5) : SpeechText.Text;
+
+            return $"{sceneID}{textID}-{retakeID}_{text}.mp3";
+        }
+
+        private void UpdateDisplayValues(string fileName)
+        {
+            if (UseAutoIncrement.IsChecked ?? false)
+            {
+                var textID = Convert.ToInt32(TextID.Text) + 1;
+                TextID.Text = $"{textID:000}";
+            }
+            
+            ResultText.Content = $"{fileName} の音声データを保存しました。";
         }
 
         private void Button_Click_Play(object sender, RoutedEventArgs e)
@@ -95,6 +149,11 @@ namespace GCP_TextToSpeech_Sample
                 _Player = new System.Media.SoundPlayer(memoryStream);
                 _Player.Play();
             }
+        }
+
+        private void Button_Click_Zero(object sender, RoutedEventArgs e)
+        {
+            TextID.Text = $"{1:000}";
         }
     }
 }
---
+++ GoogleCalendarTest/GoogleCalendarTest/Program.cs
@@ -0,0 +1,107 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GoogleCalendarTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+
+            program.Run();
+        }
+
+        private void Run()
+        {
+            // OAuth 2.0 クライアント ID の json ファイルのパス
+            var filePath = "client_id.json";
+            var scopes = new[] { CalendarService.Scope.CalendarReadonly };
+
+            // 通信設定
+            using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+                var credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)
+                ).Result;
+
+                // イベントの取得期間の指定
+                var min = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 0, 0, 0);
+                var max = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 23, 59, 59);
+
+                // メインコード
+                var events = GetCalendarEvents(credential, "*****@gmail.com", min, max);
+
+                if ((events?.Count() ?? 0) > 0)
+                {
+                    Console.WriteLine("今日の予定を表示します。");
+
+                    foreach (var eventItem in events)
+                    {
+                        var whenText = eventItem.Start.DateTime.ToString();
+
+                        if (string.IsNullOrEmpty(whenText))
+                        {
+                            whenText = eventItem.Start.Date;
+                        }
+
+                        Console.WriteLine($"{eventItem.Summary} - ({whenText})");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("今日の予定は見つかりませんでした。");
+                }
+
+            }
+        }
+
+        /// <summary>
+        /// 指定した期間のカレンダーのイベントのコレクションを取得します。
+        /// </summary>
+        /// <param name="credential">google 認証。</param>
+        /// <param name="calendarId">google カレンダー ID を表すテキスト。</param>
+        /// <param name="min">取得期間の開始。</param>
+        /// <param name="max">取得期間の終了。</param>
+        /// <param name="maxResults">最大の取得件数。</param>
+        /// <returns>カレンダーのイベントのコレクション。取得に失敗したときは null を返却します。</returns>
+        private IEnumerable<Google.Apis.Calendar.v3.Data.Event> GetCalendarEvents(ICredential credential, string calendarId, DateTime min, DateTime max, int maxResults = 10)
+        {
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Calendar Sample",
+            });
+
+            EventsResource.ListRequest request = service.Events.List(calendarId);
+
+            request.TimeMin = min;
+            request.TimeMax = max;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = maxResults;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            var events = request.Execute()?.Items;
+
+            return events;
+        }
+
+
+    }
+}
