ab9aceb7d35afa1d8bcaa328a44e04fad415a944
sh1ch <isogucchi-code@outlook.com>
2019-09-27 14:50:40 +0900

    feat: (.mdb) 形式のファイルを操作するサンプルを追加。

---
+++ MdbTest/MdbTest/AccessType.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MdbTest
+{
+    /// <summary>
+    /// <see cref="Access"/> 列挙型は、DB ファイルを作成した Access 種別を表します。
+    /// </summary>
+    public enum AccessType : int
+    {
+        /// <summary>
+        /// 未定義
+        /// </summary>
+        UnDefined = -1,
+        /// <summary>
+        /// 不明
+        /// </summary>
+        Unknown = 0,
+        /// <summary>
+        /// Access 2.0
+        /// </summary>
+        Access2 = 2,
+        /// <summary>
+        /// Access 7.0 or (Access 95)
+        /// </summary>
+        Access7_95 = 6,
+        /// <summary>
+        /// Access 97
+        /// </summary>
+        Access97 = 7,
+        /// <summary>
+        /// Access 2000
+        /// </summary>
+        Access2000 = 800,
+        /// <summary>
+        /// Access 2002
+        /// </summary>
+        Access2002 = 802,
+        /// <summary>
+        /// Access 2003
+        /// </summary>
+        Access2003 = 803,
+    }
+}
---
+++ MdbTest/MdbTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MdbTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+
+            program.Run();
+        }
+
+        public void Run()
+        {
+            var db = new Mdb();
+            
+            Console.WriteLine($"ADO Engine タイプ : {db.GetEngineType()}");
+            Console.WriteLine($"DAO Access バージョン : {db.GetAccessVersion()}");
+            Console.WriteLine($"推定 Access バージョン : {db.GetAccessType()}");
+
+            Console.WriteLine($"----");
+
+            var charactors = db.SelectFromOLEDB();
+
+            foreach (var chara in charactors)
+            {
+                Console.WriteLine($"名前:{chara.Name.TrimEnd()} HP:{chara.Hp, 4} MP:{chara.Mp, 4} 状態:{(!chara.IsDead ? "生": "死")}");
+            }
+        }
+    }
+}
