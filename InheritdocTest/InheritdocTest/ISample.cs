30b4c9a9cbfc99edc4601172dff1751f82a23aa5
sh1ch <isogucchi-code@outlook.com>
2020-01-14 15:51:56 +0900

    feat: inheritdoc を確認するサンプルを追加。

---
+++ InheritdocTest/InheritdocTest/ISample.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritdocTest
+{
+    /// <summary>
+    /// <see cref="ISample"/> インターフェースは、コメントのテストを定義します。
+    /// </summary>
+    public interface ISample
+    {
+        /// <summary>
+        /// <see cref="ISample"/> インターフェースで定義したプロパティのコメントです。
+        /// </summary>
+        string Property1 { get; set; }
+
+        /// <summary>
+        /// <see cref="ISample"/> インターフェースで定義したイベントのコメントです。
+        /// </summary>
+        event EventHandler Occurred;
+
+        /// <summary>
+        /// <see cref="ISample"/> インターフェースで定義したメソッドのコメントです。
+        /// </summary>
+        void Method1();
+    }
+}
---
+++ InheritdocTest/InheritdocTest/Sample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritdocTest
+{
+    /// <summary>
+    /// <see cref="Sample"/> クラスは、<see cref="ISample"/> インターフェースで定義したコメントを
+    /// 継承するかテストするためのクラスです。
+    /// </summary>
+    public class Sample : ISample
+    {
+        /// <inheritdoc />
+        public string Property1 { get; set; }
+
+        /// <inheritdoc />
+        public event EventHandler Occurred;
+
+        /// <inheritdoc />
+        public void Method1()
+        {
+            Occurred?.Invoke(this, EventArgs.Empty);
+        }
+    }}
