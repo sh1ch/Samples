87668edf1bdff7cc09fe13b64ce6b0a97f61851f
sh1ch <isogucchi-code@outlook.com>
2020-01-14 15:51:56 +0900

    feat: inheritdoc を確認するサンプルを追加。

---
+++ InheritdocTest/InheritdocTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritdocTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sample = new Sample();
+
+            sample.Occurred += (s, a) => 
+            {
+                var data = s as Sample;
+                Console.WriteLine($"{data?.Property1 ?? "Unk"} : Event Occurred.");
+            };
+
+            sample.Property1 = "Main";
+            sample.Method1();
+        }
+    }
+}
