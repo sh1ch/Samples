dad30af36078b547243fb869ffb632e64d4b4c98
sh1ch <isogucchi-code@outlook.com>
2020-03-18 15:58:18 +0900

    refactor: DynamicJson から System.Text.Json に変更。
    - 列番号が正しく設定されないケースに対応。

--- DataGridColumnPositionSaveSample/DataGridColumnPositionSaveSample/DataGridColumnSettings.cs
+++ DataGridColumnPositionSaveSample/DataGridColumnPositionSaveSample/DataGridColumnSettings.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 
@@ -11,34 +12,53 @@ namespace DataGridColumnPositionSaveSample
     /// <summary>
     /// <see cref="DataGridColumnSettings"/> クラスは、<see cref="System.Windows.Controls.DataGridColumn"/> の設定を管理するクラスです。
     /// </summary>
-    public class DataGridColumnSettings
+    public static class DataGridColumnSettings
     {
-        #region Properties
-
-        #endregion
-
-        #region Initializes
-
-        /// <summary>
-        /// <see cref="DataGridColumnSettings"/> クラスの新しいインスタンスを初期化します。
-        /// </summary>
-        public DataGridColumnSettings() { }
-
-        #endregion
-
         #region Public Methods
 
-        public void Save(string filePath, IEnumerable<DataGridColumn> columns)
+        public static void Load(string filePath, IEnumerable<DataGridColumn> columns)
+        {
+            var text = "";
+
+            if (!File.Exists(filePath)) throw new IOException("指定されたファイルパスにファイルが存在しません。");
+
+            using (var reader = new StreamReader(filePath, Encoding.UTF8))
+            {
+                text = reader.ReadToEnd();
+            }
+
+            if (string.IsNullOrEmpty(text)) throw new IOException("指定されたファイルのデータが空白です。");
+
+            var jsonParams = JsonSerializer.Deserialize<IEnumerable<DataGridParameter>>(text);
+
+            if ((jsonParams?.Count() ?? 0) <= 0) return;
+
+            // 左端（小さい番号）から順番に設定していく
+            foreach (DataGridParameter param in jsonParams.OrderBy(p => p.DisplayIndex))
+            {
+                var column = columns.SingleOrDefault(p => p.Header.ToString() == param.Header);
+
+                if (column != null)
+                {
+                    column.DisplayIndex = param.DisplayIndex;
+                    column.Width = param.Width;
+                }
+            }
+        }
+
+        public static void Save(string filePath, IEnumerable<DataGridColumn> columns)
         {
             if (string.IsNullOrEmpty(filePath)) throw new IOException("指定されたファイルパスに異常があります。");
             if (columns == null) throw new ArgumentNullException("設定するカラムが null です。");
 
+            var columnParams = columns.Select(p => new DataGridParameter { Header = p.Header.ToString(), DisplayIndex = p.DisplayIndex, Width = p.ActualWidth });
+            var text = JsonSerializer.Serialize(columnParams);
 
+            using (var writer = new StreamWriter(filePath, false, Encoding.UTF8))
+            {
+                writer.Write(text);
+            }
         }
-
-        #endregion
-
-        #region Private Methods
 
         #endregion
     }
