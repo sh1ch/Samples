02077b6ef8b73899fd2e3724c8dff4872f4ad591
sh1ch <isogucchi-code@outlook.com>
2020-03-18 15:11:01 +0900

    feat: データグリッドの列を記憶するサンプルを追加。

---
+++ DataGridColumnPositionSaveSample/DataGridColumnPositionSaveSample/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using Codeplex.Data;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataGridColumnPositionSaveSample
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel _VM = new MainWindowViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = _VM;
+        }
+
+        private void Window_SourceInitialized(object sender, EventArgs e)
+        {
+            var text = "";
+
+            if (!File.Exists("datagrid.json")) return;
+
+            using (var reader = new StreamReader("datagrid.json", Encoding.UTF8))
+            {
+                text = reader.ReadToEnd();
+            }
+
+            if (string.IsNullOrEmpty(text)) return;
+
+            var json = DynamicJson.Parse(text);
+            
+            foreach (DataGridParameter param in json)
+            {
+                var column = DataGrid.Columns.SingleOrDefault(p => p.Header.ToString() == param.Header);
+
+                if (column != null)
+                {
+                    column.DisplayIndex = param.DisplayIndex;
+                    column.Width = param.Width;
+                }
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var columns = DataGrid.Columns
+                .Select(p => new DataGridParameter { Header = p.Header.ToString(), DisplayIndex = p.DisplayIndex, Width = p.ActualWidth });
+
+            var text = DynamicJson.Serialize(columns);
+
+            using (var writer = new StreamWriter("datagrid.json", false, Encoding.UTF8))
+            {
+                writer.Write(text);
+            }
+        }
+    }
+}
---
+++ DataGridColumnPositionSaveSample/DataGridColumnPositionSaveSample/SampleData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridColumnPositionSaveSample
+{
+    public class SampleData
+    {
+        public int No { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public int Hp { get; set; }
+        public int Mp { get; set; }
+    }
+}
