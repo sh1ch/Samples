ad9843ceb7f0d3b65e9243cdcfc4fdc3c3972d0a
sh1ch <isogucchi-code@outlook.com>
2020-03-26 19:26:09 +0900

    feat: Xorshift RNGs による疑似乱数ジェネレーターのサンプルを追加。

---
+++ XorshiftRandom/XorshiftRandom/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace XorshiftRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+
+            Console.CursorVisible = false;
+
+            program.Run();
+
+            Console.CursorVisible = true;
+
+            Console.ReadLine();
+        }
+
+        public void Run()
+        {
+            RangeTest();
+            ImageTest();
+        }
+
+        public void ImageTest()
+        {
+            var random = new RandomState();
+
+            random.SetSeed(DateTime.Now.Ticks);
+
+            var width = 640;
+            var height = 640;
+
+            var bmp = new Bitmap(width, height);
+
+            for (var x = 0; x < width; x++)
+            {
+                for (var y = 0; y < height; y++)
+                {
+                    var r = random.Range(0, 1);
+                    var color = r == 0 ? Color.White : Color.Black;
+
+                    bmp.SetPixel(x, y, color);
+                }
+            }
+
+            bmp.Save($"image-{DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss")}.png", ImageFormat.Png);
+        }
+
+        public void RangeTest()
+        {
+            var random = new RandomState();
+            random.SetSeed(DateTime.Now.Ticks);
+            
+            // 試行回数
+            ulong count = 10000000L;
+
+            // 範囲
+            var min = 0;
+            var max = 100;
+
+            var maxW = 0.0D;
+            var minW = 1.0D;
+
+            for (ulong i = 0; i < count; i++)
+            {
+                var w = random.Range(min, max);
+
+                if (maxW < w)
+                {
+                    maxW = w;
+                }
+                else if (minW > w)
+                {
+                    minW = w;
+                }
+
+                if (i % (ulong)(count / 10000) == 0)
+                {
+                    Console.Write("{0:00.0000} %", ((double)i / (double)count * 100));
+                    Console.SetCursorPosition(0, Console.CursorTop);
+                }
+            }
+
+            Console.WriteLine($"MaxValue = {maxW}");
+            Console.WriteLine($"MinValue = {minW}");
+        }
+
+
+    }
+}
---
+++ XorshiftRandom/XorshiftRandom/Random.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XorshiftRandom
+{
+    /// <summary>
+    /// <see cref="Random"/> クラスは、Xorshift RNGs による疑似乱数ジェネレーターです。
+    /// <para>
+    /// 静的に乱数を運用するために使用します。通常、エフェクトなど再現性を考慮しない要素に利用します。
+    /// </para>
+    /// </summary>
+    public static class Random
+    {
+        #region Fields
+
+        private static RandomState _State = new RandomState();
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// 疑似乱数ジェネレーターの状態を決めるシード値を設定します。
+        /// </summary>
+        /// <param name="seed">擬似乱数系列の開始値を計算するために使用するシード値。</param>
+        public static void SetSeed(long seed)
+        {
+            _State.SetSeed((ulong)seed);
+        }
+
+        /// <summary>
+        /// 疑似乱数ジェネレーターの状態を決めるシード値を設定します。
+        /// </summary>
+        /// <param name="seed">擬似乱数系列の開始値を計算するために使用するシード値。</param>
+        public static void SetSeed(ulong seed)
+        {
+            _State.SetSeed(seed);
+        }
+
+        /// <summary>
+        /// 指定した範囲のランダムな数値を取得します。（指定した値を含む）
+        /// </summary>
+        /// <param name="minValue">最小値。</param>
+        /// <param name="maxValue">最大値。</param>
+        /// <returns>疑似乱数値。</returns>
+        public static int Range(int minValue, int maxValue)
+        {
+            return _State.Range(minValue, maxValue);
+        }
+
+        /// <summary>
+        /// 指定した範囲のランダムな数値を取得します。（指定した値を含む）
+        /// </summary>
+        /// <param name="minValue">最小値。</param>
+        /// <param name="maxValue">最大値。</param>
+        /// <returns>疑似乱数値。</returns>
+        public static double Range(double minValue = 0.0D, double maxValue = 1.0D)
+        {
+            return _State.Range(minValue, maxValue);
+        }
+
+        /// <summary>
+        /// 疑似乱数を取得します。
+        /// <para>
+        /// 乱数の範囲は、0 ≦ w ≦ (2^64) -1 です。（値を含む）
+        /// </para>
+        /// </summary>
+        /// <returns>疑似乱数値。</returns>
+        public static ulong GetNext()
+        {
+            return _State.GetNext();
+        }
+
+        #endregion
+    }
+}
