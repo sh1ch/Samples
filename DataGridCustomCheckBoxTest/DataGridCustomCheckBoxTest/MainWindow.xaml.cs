56dced0cb4565b485fe3f9f0e2f7cf3ff5495ac9
sh1ch <isogucchi-code@outlook.com>
2020-05-28 14:34:21 +0900

    feat: DataGrid 内のカスタム CheckBox の Binding のサンプル。

---
+++ DataGridCustomCheckBoxTest/DataGridCustomCheckBoxTest/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataGridCustomCheckBoxTest
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<SampleData> Samples { get; } = new ObservableCollection<SampleData>
+        {
+            new SampleData { No = 1, Text = "エルルゥ", Data1 = true, Data2 = false },
+            new SampleData { No = 2, Text = "アルルゥ", Data1 = false, Data2 = true },
+            new SampleData { No = 3, Text = "ムックル", Data1 = false, Data2 = false },
+        };
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+        }
+    }
+}
