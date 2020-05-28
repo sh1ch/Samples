778cbce5e2e107c09e5e5cdcd44943b700e40dcd
sh1ch <isogucchi-code@outlook.com>
2020-05-28 14:34:21 +0900

    feat: DataGrid 内のカスタム CheckBox の Binding のサンプル。

---
+++ DataGridCustomCheckBoxTest/DataGridCustomCheckBoxTest/SampleData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridCustomCheckBoxTest
+{
+    public class SampleData
+    {
+        public int No { get; set; }
+
+        public string Text { get; set; }
+
+        private bool _Data1;
+
+        public bool Data1
+        {
+            get => _Data1;
+            set
+            {
+                Console.WriteLine($"{nameof(Data1)} Setter Called.");
+                _Data1 = value;
+            }
+        }
+
+        private bool _Data2;
+        public bool Data2
+        {
+            get => _Data2;
+            set
+            {
+                Console.WriteLine($"{nameof(Data2)} Setter Called.");
+                _Data2 = value;
+            }
+        }
+    }
+}
---
+++ DataGridCustomCheckBoxTest/DataGridCustomCheckBoxTest/TwoCheckBox.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataGridCustomCheckBoxTest
+{
+    public class TwoCheckBox : Control
+    {
+        #region DependencyProperties
+
+        public static readonly DependencyProperty IsChecked1Property =
+            DependencyProperty.Register(
+                nameof(IsChecked1),
+                typeof(bool),
+                typeof(TwoCheckBox),
+                new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault)
+            );
+
+        public bool IsChecked1
+        {
+            get { return (bool)GetValue(IsChecked1Property); }
+            set { SetValue(IsChecked1Property, value); }
+        }
+
+        public static readonly DependencyProperty IsChecked2Property =
+            DependencyProperty.Register(
+                nameof(IsChecked2),
+                typeof(bool),
+                typeof(TwoCheckBox),
+                new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault)
+            );
+
+        public bool IsChecked2
+        {
+            get { return (bool)GetValue(IsChecked2Property); }
+            set { SetValue(IsChecked2Property, value); }
+        }
+
+        #endregion
+
+        static TwoCheckBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(TwoCheckBox), new FrameworkPropertyMetadata(typeof(TwoCheckBox)));
+        }
+    }
+}
