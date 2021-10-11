5a8f78da4a39158f9c5a991c8a0a9eec5787447d
shuichi <isogucchi@outlook.com>
2021-10-11 17:04:23 +0900

    feat: 受け渡し例を追加

--- di_sample1/di_sample1/Sample1.cs
+++ di_sample1/di_sample1/Sample1.cs
@@ -38,6 +38,25 @@ namespace di_sample1
             public void Run() => Console.WriteLine($"Run {_Vehicle.GetType().Name} - {_Vehicle.Run()} mile.");
         }
 
+        public class Driver2
+        {
+            private IVehicle _Vehicle1;
+            private IVehicle _Vehicle2;
+
+            [InjectionConstructor]
+            public Driver2(IVehicle vehicle1, IVehicle vehicle2)
+            {
+                _Vehicle1 = vehicle1;
+                _Vehicle2 = vehicle2;
+            }
+
+            public void Run()
+            {
+                Console.WriteLine($"Run2 {_Vehicle1.GetType().Name} - {_Vehicle1.Run()} mile.");
+                Console.WriteLine($"Run2 {_Vehicle2.GetType().Name} - {_Vehicle2.Run()} mile.");
+            }
+        }
+
         public void Test()
         {
             var container = new UnityContainer();
@@ -48,11 +67,23 @@ namespace di_sample1
             container.RegisterType<Driver>(nameof(Car) + nameof(Driver), new InjectionConstructor(container.Resolve<IVehicle>(nameof(Car))));
             container.RegisterType<Driver>(nameof(Bike) + nameof(Driver), new InjectionConstructor(container.Resolve<IVehicle>(nameof(Bike))));
 
+            container.RegisterType<Driver2>(
+                nameof(Driver2), 
+                new InjectionConstructor(
+                    container.Resolve<IVehicle>(nameof(Car)), 
+                    container.Resolve<IVehicle>(nameof(Bike))
+                )
+            );
+
             var car = container.Resolve<IVehicle>(nameof(Car));
+
             var carDriver = container.Resolve<Driver>(nameof(Bike) + nameof(Driver));
+            var driver2 = container.Resolve<Driver2>(nameof(Driver2));
 
             car.Run();
             carDriver.Run();
+            driver2.Run();
+            driver2.Run();
         }
 
     }
