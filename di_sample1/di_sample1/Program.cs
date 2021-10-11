92fa13466603087a1c9803aa01b0172b9a1137f6
shuichi <isogucchi@outlook.com>
2021-10-11 15:40:56 +0900

    feat: DI 例を追加

--- di_sample1/di_sample1/Program.cs
+++ di_sample1/di_sample1/Program.cs
@@ -13,11 +13,16 @@ namespace di_sample1
         {
             var sample1 = new Sample1();
             var sample2 = new Sample2();
+            var sample3 = new Sample3();
+            var sample4 = new Sample4();
 
             sample1.Test();
+            Console.WriteLine("--");
             sample2.Test();
-
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("--");
+            sample3.Test();
+            Console.WriteLine("--");
+            sample4.Test();
         }
     }
 }
