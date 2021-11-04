d2205de73e5544e99011217f9ddd3068647974ac
sh1ch <isogucchi-code@outlook.com>
2021-11-04 11:54:37 +0900

    feat: PasswordBox の Binding サンプルを追加。

---
+++ PasswordBoxTest/PasswordBoxTest/AttachedProperty.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PasswordBoxTest
+{
+    class AttachedProperty
+    {
+        private static readonly DependencyProperty IsAttachedProperty =
+            DependencyProperty.RegisterAttached(
+                "IsAttached",
+                typeof(bool),
+                typeof(AttachedProperty),
+                new FrameworkPropertyMetadata(
+                    false,
+                    FrameworkPropertyMetadataOptions.None,
+                    (s, e) =>
+                    {
+                        if (s is PasswordBox passwordBox)
+                        {
+                            if (passwordBox == null)
+                            {
+                                return;
+                            }
+
+                            if ((bool)e.OldValue)
+                            {
+                                passwordBox.PasswordChanged -= PasswordBox_PasswordChanged;
+                            }
+
+                            if ((bool)e.NewValue)
+                            {
+                                passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+                            }
+                        }
+                    })
+                );
+
+        private static readonly DependencyProperty BindablePasswordProperty =
+            DependencyProperty.RegisterAttached(
+                "BindablePassword",
+                typeof(string),
+                typeof(AttachedProperty),
+                new FrameworkPropertyMetadata(
+                    "",
+                    FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                    (s, e) =>
+                    {
+                        if (s is PasswordBox passwordBox)
+                        {
+                            var newPassword = (string)e.NewValue;
+
+                            if (GetIsAttached(passwordBox) == false)
+                            {
+                                SetIsAttached(passwordBox, true);
+                            }
+
+                            // 例外
+                            if (string.IsNullOrEmpty(passwordBox.Password) && string.IsNullOrEmpty(newPassword) ||
+                                passwordBox.Password == newPassword)
+                            {
+                                return;
+                            }
+
+                            passwordBox.PasswordChanged -= PasswordBox_PasswordChanged;
+                            passwordBox.Password = newPassword;
+                            passwordBox.PasswordChanged += PasswordBox_PasswordChanged;
+                        }
+                    })
+                );
+
+        public static bool GetIsAttached(DependencyObject d)
+        {
+            return (bool)d.GetValue(IsAttachedProperty);
+        }
+
+        public static void SetIsAttached(DependencyObject d, bool value)
+        {
+            d.SetValue(IsAttachedProperty, value);
+        }
+
+        public static string GetBindablePassword(DependencyObject d)
+        {
+            return (string)d.GetValue(BindablePasswordProperty);
+        }
+
+        public static void SetBindablePassword(DependencyObject d, string value)
+        {
+            d.SetValue(BindablePasswordProperty, value);
+        }
+
+        private static void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            var passwordBox = sender as PasswordBox;
+
+            if (passwordBox == null)
+            {
+                return;
+            }
+
+            SetBindablePassword(passwordBox, passwordBox.Password);
+        }
+    }
+}
---
+++ PasswordBoxTest/PasswordBoxTest/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordBoxTest
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _text1 = "test value1";
+        public string Text1 
+        {
+            get => _text1;
+            set => SetProperty(ref _text1, value);
+        }
+
+        private string _text2 = "test value2";
+        public string Text2
+        {
+            get => _text2;
+            set => SetProperty(ref _text2, value);
+        }
+
+        public DelegateCommand ResetTextBoxCommand { get; private set; }
+
+
+        public MainWindowViewModel()
+        {
+            ResetTextBoxCommand = new DelegateCommand(() =>
+            {
+                Text1 = "reset value";
+            });
+        }
+
+    }
+}
