2d8f317ed105ed5e4a52177a441d9c69208a38c8
sh1ch <isogucchi-code@outlook.com>
2021-11-04 11:54:37 +0900

    feat: PasswordBox の Binding サンプルを追加。

---
+++ PasswordBoxTest/PasswordBoxTest/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PasswordBoxTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+        }
+
+        protected override void ConfigureViewModelLocator()
+        {
+            base.ConfigureViewModelLocator();
+
+            ViewModelLocationProvider.Register<MainWindow, MainWindowViewModel>();
+        }
+    }
+}
