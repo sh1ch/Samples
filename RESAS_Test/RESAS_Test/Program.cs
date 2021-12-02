3c4e90c904bb6b7ffa7053b434ebee3e1209e47c
sh1ch <isogucchi-code@outlook.com>
2021-12-02 14:42:53 +0900

    feat: TextToSpeech の .NET バージョン上げ、RESAS サンプル追加

---
+++ RESAS_Test/RESAS_Test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+
+namespace RESAS_Test
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var api = Environment.GetEnvironmentVariable("RESAS_API", EnvironmentVariableTarget.User);
+            var baseUrl = "https://opendata.resas-portal.go.jp/";
+
+
+            DotNetEnv.Env.Load(".env");
+            var api2 = DotNetEnv.Env.GetString("RESAS_API");
+
+            var prefecturesApi = "api/v1/prefectures";
+            var industriesApi = "api/v1/industries/broad";
+            var url = baseUrl + industriesApi;
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("X-API-KEY", api);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var res1 = await client.GetAsync(url);
+                var res2 = await res1.Content.ReadAsStringAsync();
+
+                Console.WriteLine(res2);
+            }
+        }
+    }
+}
