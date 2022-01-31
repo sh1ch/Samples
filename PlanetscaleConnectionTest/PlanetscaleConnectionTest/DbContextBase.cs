807068927e2e7b7b3919e7ca5d5f7ebe10b3972b
sh1ch <isogucchi-code@outlook.com>
2022-01-31 17:16:19 +0900

    feat: EF に対応

---
+++ PlanetscaleConnectionTest/PlanetscaleConnectionTest/DbContextBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetscaleConnectionTest;
+
+
+public class DbContextBase : DbContext
+{
+    public DbSet<Users> Users => Set<Users>();
+
+    //public DbSet<Users> Users { get; set; } = null!;
+
+    public DbContextBase(DbConnection connection) : base (connection, true)
+    {
+
+    }
+}
