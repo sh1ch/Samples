2c8f32f0b7ed02fd13698651fd153b0bd4635b76
sh1ch <isogucchi-code@outlook.com>
2022-03-11 16:42:10 +0900

    feat: トースト通知のサンプルを追加

---
+++ ToastTest/ToastTest/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToastTest;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var imageUri = System.IO.Path.GetFullPath(@"Images\info.png");
+
+        new ToastContentBuilder()
+            .AddText("テストタイトル")
+            .AddText("テキスト")
+            .AddAppLogoOverride(new Uri(imageUri))
+            .AddButton(new ToastButton().SetContent("OK"))
+            .Show();
+    }
+
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+        ToastNotificationManagerCompat.History.Clear();
+    }
+}
