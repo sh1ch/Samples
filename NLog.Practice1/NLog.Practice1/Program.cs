cee0a38b62d0608f6b3438f600f7832b57cfb55a
sh1ch <isogucchi-code@outlook.com>
2023-01-06 16:36:16 +0900

    [feat] NLog のサンプルを追加

---
+++ NLog.Practice1/NLog.Practice1/Program.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+using NLog.Practice1;
+using System;
+
+namespace Nlog.Practice1;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Test1();
+        //Test2();
+        //Test3();
+        Test4();
+    }
+
+    static void Test1()
+    {
+        var logger = LogManager.GetCurrentClassLogger();
+
+        try
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            using var servicesProvider = new ServiceCollection()
+                .AddTransient<Runner>()
+                .AddLogging(loggingBuilder =>
+                {
+                    loggingBuilder.ClearProviders();
+                    loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                    loggingBuilder.AddNLog(config);
+                })
+                .BuildServiceProvider();
+
+            var runner = servicesProvider.GetRequiredService<Runner>();
+
+            runner.DoAction("てすと");
+
+            Console.WriteLine("Press ANY key to exit");
+            Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+            logger.Error(ex, "Stopped program because of exception");
+            throw;
+        }
+        finally
+        {
+            LogManager.Shutdown();
+        }
+    }
+
+    static void Test2()
+    {
+        var logger = LoggerFactory
+            .Create(builder => builder.AddNLog())
+            .CreateLogger<Program>();
+
+        logger.LogInformation("Program has started.");
+        Console.ReadKey();
+    }
+
+    static void Test3()
+    {
+        var logger = LogManager.GetCurrentClassLogger();
+
+        logger.Info("test.");
+    }
+
+    static void Test4()
+    {
+        var logger = LogManager.GetCurrentClassLogger();
+
+        try
+        {
+            throw new Exception("Test exception");
+        }
+        catch (Exception ex)
+        {
+            logger.Error(ex, "Got exception.");  // NLog 4.0 and newer
+        }
+    }
+}
+
---
+++ NLog.Practice1/NLog.Practice1/Runner.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLog.Practice1;
+
+public class Runner
+{
+    private readonly ILogger<Runner> _Logger;
+
+    public Runner(ILogger<Runner> logger)
+    {
+        _Logger = logger;
+    }
+
+    public void DoAction(string name)
+    {
+        _Logger.LogDebug(20, "Doing hard work! {Action}", name);
+    }
+}
