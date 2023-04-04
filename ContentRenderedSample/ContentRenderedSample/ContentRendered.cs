3abc2a2a8fc070d0fabecc538641e14701ee63c8
sh1ch <isogucchi-code@outlook.com>
2023-04-04 15:01:04 +0900

    feat: ContentRendered イベントをバインドするサンプルを追加

---
+++ ContentRenderedSample/ContentRenderedSample/ContentRendered.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using System.Diagnostics;
+using System.Windows.Controls;
+
+namespace ContentRenderedSample;
+
+/// <summary>
+/// <see cref="ContentRendered"/> クラスは、<see cref="Window.ContentRendered"/> イベントを <see cref="UserControl"/> クラスで利用するための添付プロパティを定義するクラスです。
+/// </summary>
+public class ContentRendered
+{
+    private static readonly DependencyProperty CommandProperty =
+        DependencyProperty.RegisterAttached(
+            "Command",
+            typeof(ICommand),
+            typeof(ContentRendered),
+            new FrameworkPropertyMetadata(
+                null,
+                FrameworkPropertyMetadataOptions.None,
+                (s, e) =>
+                {
+                    if (s is UserControl control)
+                    {
+                        if (control == null)
+                        {
+                            Debug.WriteLine($"{typeof(ContentRendered)}.{CommandProperty}: control is null.");
+                            return;
+                        }
+
+                        var window = Window.GetWindow(control);
+
+                        if (window == null)
+                        {
+                            Debug.WriteLine($"{typeof(ContentRendered)}.{CommandProperty}: parent window is null.");
+                            return;
+                        }
+
+                        window.ContentRendered += (sender, args) =>
+                        {
+                            var command = GetCommand(control);
+
+                            command?.Execute(e);
+                        };
+                    }
+                })
+            );
+
+    public static ICommand GetCommand(DependencyObject d)
+    {
+        return (ICommand)d.GetValue(CommandProperty);
+    }
+
+    public static void SetCommand(DependencyObject d, ICommand value)
+    {
+        d.SetValue(CommandProperty, value);
+    }
+}
+
---
+++ ContentRenderedSample/ContentRenderedSample/MainWindowViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ContentRenderedSample;
+
+public class MainWindowViewModel
+{
+    public ICommand ContentRenderedCommand { get; }
+
+    public MainWindowViewModel()
+    {
+        ContentRenderedCommand = new Command(() =>
+        {
+            MessageBox.Show($"{ContentRenderedCommand} is executed.");
+        });
+    }
+}
+
+public class Command : ICommand
+{
+    private Action _Action;
+    public bool CanExecute(object? parameter) => true;
+    public event EventHandler? CanExecuteChanged;
+
+    public Command(Action action)
+    {
+        _Action = action;
+    }
+
+    public void Execute(object? parameter)
+    {
+        _Action?.Invoke();
+    }
+}
