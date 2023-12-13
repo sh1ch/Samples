5e7d98fa48fcb6d839b55e939244348cfcea4919
sh1ch <isogucchi-code@outlook.com>
2023-12-13 11:33:59 +0900

    feat: TimeProvider のサンプルを追加

---
+++ TimeProviderTest/TestProject/UnitTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Runtime.InteropServices.JavaScript;
+using TimeProviderTest;
+
+namespace TestProject;
+
+public class Tests
+{
+	public class NoonTimeProvider : TimeProvider
+	{
+		public override DateTimeOffset GetUtcNow()
+		{
+			return new DateTimeOffset(2023, 12, 1, 3, 0, 0, TimeSpan.Zero);
+		}
+
+		public override TimeZoneInfo LocalTimeZone => 
+			TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
+	}
+
+	[SetUp]
+	public void Setup()
+	{
+	}
+
+	[Test]
+	public void TestTimeProvider()
+	{
+		var testTimeProvider = new NoonTimeProvider();
+
+		var testService = new TimeService(testTimeProvider);
+		var isNoon = testService.IsNoon();
+
+		Assert.IsTrue(isNoon);
+	}
+
+	[Test]
+	public void TestITimer()
+	{
+		var testTimeProvider = new FakeTimeProvider();
+		var result = false;
+
+		var itimer = testTimeProvider.CreateTimer(
+			_ =>
+			{
+				// 時間経過後にすること
+				result = true;
+			}, 
+			state:null, 
+			dueTime: TimeSpan.FromSeconds(1000),
+			period: Timeout.InfiniteTimeSpan);
+
+		testTimeProvider.Advance(TimeSpan.FromSeconds(1000));
+
+		Assert.True(result);
+
+		ITimer
+	}
+}
---
+++ TimeProviderTest/TimeProviderTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TimeProviderTest; 
+
+public class Program
+{
+	static void Main(string[] args)
+	{
+		var program = new Program();
+
+		program.Run(args);
+	}
+
+	public void Run(string[] args)
+	{
+		var now = TimeProvider.System.GetLocalNow();
+		var utcNow = TimeProvider.System.GetUtcNow();
+
+		Console.WriteLine(now);
+		Console.WriteLine(utcNow);
+
+		var now2 = DateTime.Now;
+		var utcNow2 = DateTimeOffset.UtcNow;
+		
+		Console.WriteLine(now2);
+		Console.WriteLine(utcNow2);
+	}
+}
+
+public class TimeService
+{
+	private readonly TimeProvider _TimeProvider;
+
+	public TimeService(TimeProvider timeProvider) => _TimeProvider = timeProvider;
+
+	public bool IsNoon()
+	{
+		var now = _TimeProvider.GetLocalNow();
+		
+		return now.Hour == 12;
+	}
+}
