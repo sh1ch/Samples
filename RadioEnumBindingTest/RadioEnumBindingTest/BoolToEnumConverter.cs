13a115e6e15d4457ae856eaa99124125feb4e441
sh1ch <isogucchi-code@outlook.com>
2024-05-20 16:32:16 +0900

    feat: RadioButton Enum の Binding サンプル

---
+++ RadioEnumBindingTest/RadioEnumBindingTest/BoolToEnumConverter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RadioEnumBindingTest;
+
+public class BoolToEnumConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		if (value == null || parameter == null)
+		{
+			return DependencyProperty.UnsetValue;
+		}
+
+		if (Enum.IsDefined(value.GetType(), value) == false)
+		{
+			return DependencyProperty.UnsetValue;
+		}
+
+		var isResult = false;
+		var canParse = Enum.TryParse(value.GetType(), parameter.ToString(), out object? result);
+
+		if (canParse && result != null)
+		{
+			isResult = (int)result == (int)value;
+		}
+		else
+		{
+			isResult = false;
+		}
+
+		return isResult;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		var parameterText = parameter?.ToString() ?? "";
+
+		if (parameterText == "")
+		{
+			return DependencyProperty.UnsetValue;
+		}
+
+		return Enum.Parse(targetType, parameterText);
+	}
+}
---
+++ RadioEnumBindingTest/RadioEnumBindingTest/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioEnumBindingTest;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+	[ObservableProperty]
+	[NotifyPropertyChangedFor(nameof(Text))]
+	private SampleEnum _sample = SampleEnum.A;
+
+	public string Text => $"{Sample}.";
+
+	/// <summary>
+	/// <see cref="MainWindowViewModel"/> クラスの新しいインスタンスを初期化します。
+	/// </summary>
+	public MainWindowViewModel()
+	{
+	}
+}
